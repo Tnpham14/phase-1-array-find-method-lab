94ccea8d8dcc0a991cc42e405c24440666d90d25
Tony Pham <tnpham@valdosta.edu>
2022-07-15 12:50:21 -0400

    commit tree b4caf3081a65d6483d828b40e7dbed8d3d97af2c

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // code your solution here
+
+function superbowlWin(array) {
+    const firstWinObj = array.find(obj => obj.result == "W");
+    if(firstWinObj != undefined) return firstWinObj.year;
+
+    return firstWinObj;
+}
